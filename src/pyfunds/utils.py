66ec3b8e072a76f3f8da8251dcba65460aa7f243
Jose Maria <melopsitaco@gmail.com>
2020-12-04 21:30:40 +0100

    Util var

--- src/pyfunds/utils.py
+++ src/pyfunds/utils.py
@@ -1,34 +1,38 @@
 import numpy as np
 import pandas as pd
 
+
 def _roi(x):
-    return x[-1]/x[0]
+    return x[-1] / x[0]
 
-def _calc_window_function(df_fund,num_days,func):
 
-    df_fund=df_fund.set_index('date')
-    df_fund = df_fund.sort_values(by='date',axis='index',ascending=True)
-    cols = df_fund.columns[df_fund.columns!='date']
+def _calc_window_function(df_fund, num_days, func):
+    df_fund = df_fund.sort_values(by='date', axis='index', ascending=True)
+    cols = df_fund.columns[df_fund.columns != 'date']
 
-    df_w=pd.concat([df_fund[col].rolling(window=num_days).apply(_roi,raw=True) for col in cols],axis=1)
-    df_w=df_w.dropna(how='all')
+    df_w = pd.concat([df_fund[col].rolling(window=num_days).apply(func, raw=True) for col in cols], axis=1)
+    df_w = df_w.dropna(how='all')
     return df_w
 
 
 def calc_roi(df_fund, num_days=365):
-    df_roi=_calc_window_function(df_fund,num_days,_roi)
+    df_roi = _calc_window_function(df_fund, num_days, _roi)
     return df_roi
 
+
 def calc_var(df_fund, num_days=365):
-    pass
+    df_roi = _calc_window_function(df_fund, num_days, _roi)
+    df_var = _calc_window_function(df_roi, num_days, np.nanvar)
+    return df_var
+
 
 def calc_annual_return(df_fund):
     df_roi = calc_roi(df_fund)
-    df_annual_roi=df_roi.groupby(df_roi.index.year).last()
+    df_annual_roi = df_roi.groupby(df_roi.index.year).last()
     return df_annual_roi
 
-def calc_annual_varianze(df_fund):
-    df_roi = calc_roi(df_fund)
-    df_annual_roi=df_roi.groupby(df_roi.index.year).var()
-    return df_annual_roi
 
+def calc_annual_var(df_fund, period_days=2):
+    df_roi = calc_roi(df_fund, num_days=period_days)
+    df_annual_roi = df_roi.groupby(df_roi.index.year).var()
+    return df_annual_roi
--- src/test/test_utils.py
+++ src/test/test_utils.py
@@ -1,35 +1,55 @@
 import pytest
 import pandas as pd
 import numpy as np
-from pyfunds.utils import calc_roi, calc_annual_return
+from pyfunds.utils import calc_roi, calc_var, calc_annual_return, calc_annual_var
+
 
 @pytest.fixture
 def df_fake():
-    df_fake=pd.DataFrame({'date':pd.date_range(start='2010-01-07',end='2020-12-31',freq='D'),
-                     'fund1':1.0001, 'fund2':30})
-    df_fake['fund3']=np.random.normal(loc=15,scale=1,size=df_fake.shape[0])
-    df_fake['fund4']=df_fake["fund1"].cumprod()    
+    df_fake = pd.DataFrame({'date': pd.date_range(start='2010-01-07', end='2020-12-31', freq='D'),
+                            'fund1': 1.0001, 'fund2': 30})
+    df_fake['fund3'] = np.random.normal(loc=15, scale=1, size=df_fake.shape[0])
+    df_fake['fund4'] = df_fake["fund1"].cumprod()
+    df_fake = df_fake.set_index('date')
     return df_fake
 
 
 def test_roi(df_fake):
-    df_roi=calc_roi(df_fake)
-    assert df_fake.shape == (4012,5)
-    assert df_roi.shape  == (4012-364,4)
-    assert all(df_roi['fund1']==1)
-    assert all(df_roi['fund2']==1)
-    assert df_roi['fund3'].mean()<1.01
-    assert df_roi['fund3'].var()>0
-    assert pytest.approx(df_roi['fund4'].var(),1e-10)==0
-    assert pytest.approx(df_roi.loc['2015-01-31','fund4'],0.01) == 1.037
+    df_roi = calc_roi(df_fake)
+    assert df_fake.shape == (4012, 4)
+    assert df_roi.shape == (4012 - 364, 4)
+    assert all(df_roi['fund1'] == 1)
+    assert all(df_roi['fund2'] == 1)
+    assert df_roi['fund3'].mean() < 1.01
+    assert df_roi['fund3'].var() > 0
+    assert pytest.approx(df_roi['fund4'].var(), 1e-10) == 0
+    assert pytest.approx(df_roi.loc['2015-01-31', 'fund4'], 0.01) == 1.037
+
+
+def test_var(df_fake):
+    df_var = calc_var(df_fake)
+    assert df_fake.shape == (4012, 4)
+    assert df_var.shape == (4012 - 364 - 364, 4)
+    assert all(df_var['fund1'] == 0)
+    assert all(df_var['fund2'] == 0)
+    assert df_var['fund3'].mean() < 1.2
+    assert pytest.approx(df_var.loc['2015-01-31', 'fund4'], 1e-10) == 0
+
 
 def test_annual_return(df_fake):
-    df_return=calc_annual_return(df_fake)
-    assert all(df_return['fund1']==1)
-    assert all(df_return['fund2']==1)
-    assert df_return['fund3'].mean()>0.7
-    assert df_return['fund3'].var()>0
-    assert pytest.approx(df_return['fund4'].var(),1e-10)==0
-    assert pytest.approx(df_return.loc[2015,'fund4'],0.01) == 1.037
+    df_return = calc_annual_return(df_fake)
+    assert all(df_return['fund1'] == 1)
+    assert all(df_return['fund2'] == 1)
+    assert df_return['fund3'].mean() > 0.7
+    assert df_return['fund3'].var() > 0
+    assert pytest.approx(df_return['fund4'].var(), 1e-10) == 0
+    assert pytest.approx(df_return.loc[2015, 'fund4'], 0.01) == 1.037
 
-
+def test_annual_var(df_fake):
+    df_var = calc_annual_var(df_fake)
+    assert all(df_var['fund1'] == 0)
+    assert all(df_var['fund2'] == 0)
+    assert df_var['fund3'].mean() > 0
+    assert df_var['fund3'].var() > 0
+    assert pytest.approx(df_var.loc[2015, 'fund4'], 0.01) == 0
+    assert all(df_var['fund4'] == 0)
